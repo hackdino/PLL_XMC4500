38b0a259dfe7ba4f699c91fdcd43bc9b30afe6bc
Thomas Biacsics <thomas@biacsics.at>
2017-06-07 19:58:52 +0200

    - added UART (not finished, invalid pins), improved main.c

---
+++ XMC4500_PLL_Audioozillator/include/BSP_ccu4.h
@@ -0,0 +1,13 @@
+/*
+ * BSP_ccu4.h
+ *
+ *  Created on: 07. Juni 2017
+ *      Author: thomas
+ */
+
+#ifndef INCLUDE_BSP_CCU4_H_
+#define INCLUDE_BSP_CCU4_H_
+
+
+
+#endif /* INCLUDE_BSP_CCU4_H_ */
--- XMC4500_PLL_Audioozillator/main.c
+++ XMC4500_PLL_Audioozillator/main.c
@@ -30,17 +30,27 @@
  *
  */
 
+
 #include <xmc_gpio.h>
 #include <xmc_dac.h>
 #include <xmc_scu.h>
 #include <xmc_eru.h>
 
+#include <BSP_uart.h>
+
 /******************************************************************** DEFINES */
 #define LED2 P1_0
-#define BUTTON2 P1_15
-#define TEST 1
+// #define BUTTON2 P1_15
+
+
+#define PLL_INPUT_PIN   P2_1
+#define PLL_OUTPUT_PIN  P14_8
+
+// UART
+#define RX_BUFFER_SIZE 64
 /******************************************************************** GLOBALS */
 const uint8_t pattern[] = XMC_DAC_PATTERN_SINE;
+
 
 /******************************************************************** STRUCTURES */
 // DAC config
@@ -63,8 +73,8 @@ XMC_DAC_CH_CONFIG_t const ch_config1=
 // ERU config
 XMC_ERU_ETL_CONFIG_t button_event_generator_config =
 {
-  .input_a = ERU1_ETL1_INPUTA_P1_15,
-  .source = XMC_ERU_ETL_SOURCE_A,
+  .input_a = ERU1_ETL0_INPUTB_P2_1,
+  .source = XMC_ERU_ETL_SOURCE_B,
   .edge_detection = XMC_ERU_ETL_EDGE_DETECTION_RISING,
   .status_flag_mode = XMC_ERU_ETL_STATUS_FLAG_MODE_HWCTRL,
   .enable_output_trigger = true,
@@ -78,29 +88,56 @@ XMC_ERU_OGU_CONFIG_t button_event_detection_config =
 
 /******************************************************************** PROTOTYPES */
 void _initDAC0(void);
-void _dac_set_value(const uint16_t dacVal);
-uint8_t _dac_set_pattern(const uint32_t freq);
+void _dac_set_value(const uint8_t channel, const uint16_t dacVal);
+uint8_t _dac_set_pattern(const uint8_t channel, const uint32_t freq);
 
 /******************************************************************** INTERRUPTS */
 void ERU1_0_IRQHandler(void)
 {
   XMC_GPIO_ToggleOutput(LED2);
+  XMC_GPIO_ToggleOutput(PLL_OUTPUT_PIN);
+
+  // TODO:
+  /*
+   * - receive PLL request from UART
+   * - on rising edge start a ccu4 timer
+   * - on next rising edge stop the timer and save the timer period
+   * -- start the DAC with the requested frequency
+   */
+
+//  if (IsPllRequest)
+//  {
+//
+//  }
 }
 
 /******************************************************************** MAIN */
 int main(void)
 {
+  char rx_buff[RX_BUFFER_SIZE] = {0};
 
-  XMC_ERU_ETL_Init(ERU1_ETL1, &button_event_generator_config);
+  XMC_ERU_ETL_Init(ERU1_ETL0, &button_event_generator_config);
   XMC_ERU_OGU_Init(ERU1_OGU0, &button_event_detection_config);
 
+  XMC_GPIO_SetMode(PLL_OUTPUT_PIN, XMC_GPIO_MODE_OUTPUT_PUSH_PULL);
   XMC_GPIO_SetMode(LED2, XMC_GPIO_MODE_OUTPUT_PUSH_PULL);
-  XMC_GPIO_SetMode(BUTTON2, XMC_GPIO_MODE_INPUT_TRISTATE);
+  XMC_GPIO_SetMode(PLL_INPUT_PIN, XMC_GPIO_MODE_INPUT_TRISTATE);
 
   NVIC_SetPriority(ERU1_0_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 63, 0));
   NVIC_EnableIRQ(ERU1_0_IRQn);
 
   _initDAC0();
+  _initUART0_CH0();
+
+
+  /* Available UART functions */
+//  _uart_send_char('X');
+//  _uart_send_string("Testing...\n");
+//  _uart_printf("Magic Number = %d\n",42);
+//
+//  _uart_get_string (rx_buff);
+//  _uart_printf("RX string = %s\n", rx_buff);
+
 
   while(1)
   {
@@ -114,7 +151,7 @@ void _initDAC0(void)
 {
 
   /* API to initial DAC Module*/
-  XMC_DAC_CH_Init(XMC_DAC0, 0U, &ch_config0);
+  //XMC_DAC_CH_Init(XMC_DAC0, 0U, &ch_config0);
   XMC_DAC_CH_Init(XMC_DAC0, 1U, &ch_config1);
 
   /* API to initial DAC in Pattern mode. When using a predefined pattern a type cast avoid warnings */
@@ -125,18 +162,18 @@ void _initDAC0(void)
 
 }
 
-void _dac_set_value(const uint16_t dacVal)
+void _dac_set_value(const uint8_t channel, const uint16_t dacVal)
 {
   /* API to write a value into DAC Output */
-  XMC_DAC_CH_Write(XMC_DAC0, 1, dacVal);
+  XMC_DAC_CH_Write(XMC_DAC0, channel, dacVal);
 
   return;
 }
 
-uint8_t _dac_set_pattern(const uint32_t freq)
+uint8_t _dac_set_pattern(const uint8_t channel, const uint32_t freq)
 {
   /* update frequency */
-  XMC_DAC_CH_STATUS_t ret = XMC_DAC_CH_SetPatternFrequency(XMC_DAC0, 0, freq);
+  XMC_DAC_CH_STATUS_t ret = XMC_DAC_CH_SetPatternFrequency(XMC_DAC0, channel, freq);
 
   if (ret == XMC_DAC_CH_STATUS_OK)
     return 0;
